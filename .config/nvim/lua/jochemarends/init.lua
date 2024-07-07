3a0a37c5c2ec50cdb7899835e6b97e819f06742a
Jochem Arends <77842807+jochemarends@users.noreply.github.com.>
2024-07-07 21:08:50 +0200

    refactor nvim config

--- .config/nvim/lua/jochemarends/init.lua
+++ .config/nvim/lua/jochemarends/init.lua
@@ -1,1 +1,16 @@
-print("hello from arends")
+require("jochemarends.remap")
+require("jochemarends.lazy")
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.wildmenu = true
+
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.smarttab = true
+vim.opt.softtabstop = 4
+vim.opt.tabstop = 4
+
+vim.opt.clipboard = "unnamedplus"
+
---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,17 @@
+return {
+    {
+        "rafi/awesome-vim-colorschemes",
+        config = function()
+            vim.opt.termguicolors = true
+            vim.cmd("colorscheme jellybeans")
+        end,
+    }
+}
+--
+-- mountaineer
+-- lunaperche
+-- hybrid_reverse
+-- paramount
+-- jellybeans
+-- pablo
+
